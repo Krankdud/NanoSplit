650825b1bc9f6b8d2a63555869b358999611c7bc
charlie <charlieschuy@gmail.com>
2018-11-16 15:05:32 -0800

    Off canvas menu

---
+++ src/menu/Menu.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import "./Menu.css";
+
+interface IMenuState {
+  isOpen: boolean;
+}
+
+class Menu extends React.Component<{}, IMenuState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+
+    this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  public openMenu() {
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  public closeMenu() {
+    this.setState({
+      isOpen: false
+    });
+  }
+
+  public render() {
+    const sidenavClass = this.state.isOpen
+      ? "sidenav sidenav-active"
+      : "sidenav sidenav-inactive";
+    return (
+      <div>
+        <div className={sidenavClass}>
+          <div className="sidenav-close" onClick={this.closeMenu}>
+            &times;
+          </div>
+          <div className="sidenav-item">New splits</div>
+          <div className="sidenav-item">Edit splits</div>
+          <div className="sidenav-item">Import</div>
+          <div className="sidenav-item">Export</div>
+          <div className="sidenav-item">Settings</div>
+        </div>
+        <span onClick={this.openMenu}>Menu</span>
+      </div>
+    );
+  }
+}
+
+export default Menu;
